3dbcb0a4867f55fb685e8b59f63effa312d64ff9
Alois Sečkár <ellrohir@seznam.cz>
2025-05-14 22:15:48 +0200

    refactor: organize server-side helper functions

--- src/runtime/server/utils/neonSQL.ts
+++ src/runtime/server/utils/neonSQL.ts
@@ -1,7 +1,11 @@
 import type { NeonQueryFunction } from '@neondatabase/serverless'
 import type { NeonTableQuery, NeonWhereQuery, NeonOrderQuery } from '../../utils/neonTypes'
 import type { NeonDriverResult } from './getNeonClient'
-import { sanitizeSQLString } from './sanitizeSQL'
+import { sanitizeSQLString } from './helpers/sanitizeSQL'
+import {
+  getColumnsClause, getGroupByClause, getHavingClause,
+  getLimitClause, getOrderClause, getTableClause, getWhereClause,
+} from './helpers/neonSQLHelpers'
 
 // separate wrapper instead of forcing users to pass 'count(*)' as column name
 export async function count(neon: NeonQueryFunction<boolean, boolean>, from: string | NeonTableQuery | NeonTableQuery[], where?: string | NeonWhereQuery | NeonWhereQuery[]) {
@@ -73,156 +77,4 @@ export async function del(neon: NeonQueryFunction<boolean, boolean>, table: stri
 
   // passing in "queryOpts" (matching with defaults) to fullfill TypeScript requirements
   return await neon.query(sqlString, undefined, { arrayMode: false, fullResults: false })
-}
-
-function getTableClause(from: string | NeonTableQuery | NeonTableQuery[]): string {
-  let sqlString = ' FROM '
-  if (typeof from === 'string') {
-    sqlString += from
-  }
-  else if (Array.isArray(from)) {
-    let tables = ''
-    from.forEach((t) => {
-      if (tables) {
-        tables += ` JOIN ${tableWithSchema(t)} ${t.alias} ON ${t.joinColumn1} = ${t.joinColumn2}`
-      }
-      else {
-        tables = `${tableWithSchema(t)} ${t.alias}`
-      }
-    })
-    sqlString += tables
-  }
-  else {
-    sqlString += tableWithSchema(from)
-  }
-  return sqlString
-}
-
-// include schema name if specified
-function tableWithSchema(t: NeonTableQuery): string {
-  if (t.schema) {
-    return `${t.schema}.${t.table}`
-  }
-  return t.table
-}
-
-function getColumnsClause(columns: string | string[]): string {
-  let sqlString = ''
-  if (Array.isArray(columns)) {
-    sqlString += columns.join(', ')
-  }
-  else {
-    sqlString += columns
-  }
-  return sqlString
-}
-
-function getWhereClause(where?: string | NeonWhereQuery | NeonWhereQuery[]): string {
-  let sqlString = ''
-  if (where) {
-    sqlString += ' WHERE '
-    if (typeof where === 'string') {
-      sqlString += where
-    }
-    else if (Array.isArray(where)) {
-      let conditions = ''
-      where.forEach((w) => {
-        if (conditions) {
-          conditions += ` ${w.operator} ${w.column} ${w.condition} ${escapeIfNeeded(w.value)}`
-        }
-        else {
-          conditions = `${w.column} ${w.condition} ${escapeIfNeeded(w.value)}`
-        }
-      })
-      sqlString += conditions
-    }
-    else {
-      sqlString += `${where.column} ${where.condition} ${escapeIfNeeded(where.value)}`
-    }
-  }
-  return sqlString
-}
-
-function getOrderClause(order?: string | NeonOrderQuery | NeonOrderQuery[]): string {
-  let sqlString = ''
-  if (order) {
-    sqlString = ' ORDER BY '
-    if (typeof order === 'string') {
-      sqlString += order
-    }
-    else if (Array.isArray(order)) {
-      let ordering = ''
-      order.forEach((o) => {
-        if (ordering) {
-          ordering += `, ${o.column} ${o.direction || 'ASC'}`
-        }
-        else {
-          ordering = `${o.column} ${o.direction || 'ASC'}`
-        }
-      })
-      sqlString += ordering
-    }
-    else {
-      sqlString += `${order.column} ${order.direction || 'ASC'}`
-    }
-  }
-  return sqlString
-}
-
-function getGroupByClause(group?: string | string[]): string {
-  let sqlString = ''
-  if (group) {
-    sqlString = ' GROUP BY '
-    if (Array.isArray(group)) {
-      sqlString += group.join(', ')
-    }
-    else {
-      sqlString += group
-    }
-  }
-  return sqlString
-}
-
-function getHavingClause(having?: string | NeonWhereQuery | NeonWhereQuery[]): string {
-  let sqlString = ''
-  if (having) {
-    sqlString += ' HAVING '
-    if (typeof having === 'string') {
-      sqlString += having
-    }
-    else if (Array.isArray(having)) {
-      let conditions = ''
-      having.forEach((h) => {
-        if (conditions) {
-          conditions += ` ${h.operator} ${h.column} ${h.condition} ${escapeIfNeeded(h.value)}`
-        }
-        else {
-          conditions = `${h.column} ${h.condition} ${escapeIfNeeded(h.value)}`
-        }
-      })
-      sqlString += conditions
-    }
-    else {
-      sqlString += `${having.column} ${having.condition} ${escapeIfNeeded(having.value)}`
-    }
-  }
-  return sqlString
-}
-
-function getLimitClause(limit?: number): string {
-  let sqlString = ''
-  if (limit) {
-    sqlString += ` LIMIT ${limit}`
-  }
-  return sqlString
-}
-
-function escapeIfNeeded(value: unknown) {
-  if (typeof value === 'string' && !value.startsWith('\'')) {
-    value = '\'' + value
-  }
-  if (typeof value === 'string' && !value.endsWith('\'')) {
-    value = value + '\''
-  }
-  return value
 }
