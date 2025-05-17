23b7a4cc2985b59635b4327f1fb6c8d1a2dd7c4d
Alois Sečkár <ellrohir@seznam.cz>
2025-05-17 22:29:26 +0200

    test: break SELECT suite into separate tests

--- test/02-select.test.ts
+++ test/02-select.test.ts
@@ -1,6 +1,13 @@
 import { fileURLToPath } from 'node:url'
-import { describe, test, expect } from 'vitest'
+import { describe, test } from 'vitest'
 import { setup, createPage, url } from '@nuxt/test-utils/e2e'
+
+const getPage = async (pageName: string) => {
+  // render in browser
+  const page = await createPage()
+  await page.goto(url(`/${pageName}`), { waitUntil: 'hydration' })
+  return page
+}
 
 describe('nuxt-neon SELECT test suite', async () => {
   await setup({
@@ -8,13 +15,8 @@ describe('nuxt-neon SELECT test suite', async () => {
   })
 
   // various test queries defined in /test/fixtures/select/components/*.vue
-  test('SELECT QUERY SET performed correctly', async () => {
-    // render in browser
-    const page = await createPage()
-    await page.goto(url('/'), { waitUntil: 'hydration' })
-
-    // check test query results
-
+  test('SELECT TEST 1 performed correctly', async ({ expect }) => {
+    const page = await getPage('TestData1')
     const dataDiv1 = page.locator('#data-1')
     const dataHtml1 = await dataDiv1.innerHTML()
     expect(dataHtml1).toContain('id')
@@ -22,7 +24,10 @@ describe('nuxt-neon SELECT test suite', async () => {
     expect(dataHtml1).toContain('"value_bool": false')
     const idsIn1 = (dataHtml1.match(/"id"/g) || []).length
     expect(idsIn1).toBe(10)
+  })
 
+  test('SELECT TEST 2 performed correctly', async ({ expect }) => {
+    const page = await getPage('TestData2')
     const dataDiv2 = page.locator('#data-2')
     const dataHtml2 = await dataDiv2.innerHTML()
     expect(dataHtml2).toContain('"id": 4')
@@ -31,42 +36,60 @@ describe('nuxt-neon SELECT test suite', async () => {
     expect(dataHtml2).toContain('"value_bool": true')
     const idsIn2 = (dataHtml2.match(/"id"/g) || []).length
     expect(idsIn2).toBe(1)
+  })
 
+  test('SELECT TEST 3 performed correctly', async ({ expect }) => {
+    const page = await getPage('TestData3')
     const dataDiv3 = page.locator('#data-3')
     const dataHtml3 = await dataDiv3.innerHTML()
     expect(dataHtml3).toContain('"name": "c4ca4238a0"')
     expect(dataHtml3).toContain('"name": "a87ff679a2"')
     const idsIn3 = (dataHtml3.match(/"id"/g) || []).length
     expect(idsIn3).toBe(9)
+  })
 
+  test('SELECT TEST 4 performed correctly', async ({ expect }) => {
+    const page = await getPage('TestData4')
     const dataDiv4 = page.locator('#data-4')
     const dataHtml4 = await dataDiv4.innerHTML()
     expect(dataHtml4).toContain('"name": "c4ca4238a0"')
     expect(dataHtml4).toContain('"bool_value": "Text value for true"')
     const idsIn4 = (dataHtml4.match(/"id"/g) || []).length
     expect(idsIn4).toBe(3)
+  })
 
+  test('SELECT TEST 5 performed correctly', async ({ expect }) => {
+    const page = await getPage('TestData5')
     const dataDiv5 = page.locator('#data-5')
     const dataHtml5 = await dataDiv5.innerHTML()
     expect(dataHtml5).toContain('"name": "c4ca4238a0"')
     expect(dataHtml5).toContain('"name": "a87ff679a2"')
     const idsIn5 = (dataHtml5.match(/"id"/g) || []).length
     expect(idsIn5).toBe(2)
+  })
 
+  test('SELECT TEST 6 performed correctly', async ({ expect }) => {
+    const page = await getPage('TestData6')
     const dataDiv6 = page.locator('#data-6')
     const dataHtml6 = await dataDiv6.innerHTML()
     expect(dataHtml6).toContain('"id": 6')
     expect(dataHtml6).toContain('"id": 9')
     const idsIn6 = (dataHtml6.match(/"id"/g) || []).length
     expect(idsIn6).toBe(5)
+  })
 
+  test('SELECT TEST 7 performed correctly', async ({ expect }) => {
+    const page = await getPage('TestData7')
     const dataDiv7 = page.locator('#data-7')
     const dataHtml7 = await dataDiv7.innerHTML()
     expect(dataHtml7).toContain('"name": "neon2-1"')
     expect(dataHtml7).toContain('"value": 0.74727505')
     const idsIn7 = (dataHtml7.match(/"id"/g) || []).length
     expect(idsIn7).toBe(5)
+  })
 
+  test('SELECT TEST 8 performed correctly', async ({ expect }) => {
+    const page = await getPage('TestData8')
     const dataDiv8 = page.locator('#data-8')
     const dataHtml8 = await dataDiv8.innerHTML()
     expect(dataHtml8).toContain('"name": "neon2-1"')
@@ -74,7 +97,10 @@ describe('nuxt-neon SELECT test suite', async () => {
     expect(dataHtml8).toContain('"bool_value": "Text value for true"')
     const idsIn8 = (dataHtml8.match(/"id"/g) || []).length
     expect(idsIn8).toBe(3)
+  })
 
+  test('SELECT TEST 9 performed correctly', async ({ expect }) => {
+    const page = await getPage('TestData9')
     const dataDiv9 = page.locator('#data-9')
     const dataHtml9 = await dataDiv9.innerHTML()
     expect(dataHtml9).toContain('"name": "c4ca4238a0"')
--- test/fixtures/select/app.vue
+++ test/fixtures/select/app.vue
@@ -1,27 +1,4 @@
 <template>
   <h1>Nuxt-Neon SELECT TEST</h1>
-  <TestData1 />
-  <TestData2 />
-  <TestData3 />
-  <TestData4 />
-  <TestData5 />
-  <TestData6 />
-  <TestData7 />
-  <TestData8 />
-  <TestData9 />
+  <NuxtPage />
 </template>
-
-<script setup lang="ts">
-// this fixture is used to test various SELECT features
-// like using JOIN from more tables or querying with WHERE clauses
-
-import TestData1 from './components/TestData1.vue'
-import TestData2 from './components/TestData2.vue'
-import TestData3 from './components/TestData3.vue'
-import TestData4 from './components/TestData4.vue'
-import TestData5 from './components/TestData5.vue'
-import TestData6 from './components/TestData6.vue'
-import TestData7 from './components/TestData7.vue'
-import TestData8 from './components/TestData8.vue'
-import TestData9 from './components/TestData9.vue'
-</script>
