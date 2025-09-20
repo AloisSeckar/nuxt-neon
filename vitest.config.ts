408686ec7b0b809e656cf8410ba26c3d22bf72ab
Alois Sečkár <ellrohir@seznam.cz>
2025-09-20 07:33:17 +0200

    build: adjust config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vitest/config'
+import { loadVitestConfig } from 'nuxt-spec/config'
 
-export default defineConfig({
+export default loadVitestConfig({
   test: {
     // run test files strictly one after another
     fileParallelism: false,
