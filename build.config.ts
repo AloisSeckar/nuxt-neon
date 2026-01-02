1324db456a3363e535e1c72caa0174609f098e5a
Alois Sečkár <ellrohir@seznam.cz>
2026-01-02 10:15:37 +0100

    build: exclude server tsconfig.json from module output

--- build.config.ts
+++ build.config.ts
@@ -1,18 +1,25 @@
-// minify the dist files
 import { defineBuildConfig } from 'unbuild'
+import { rm } from 'node:fs/promises'
+import { resolve } from 'node:path'
 
 export default defineBuildConfig({
+  // minify module.mjs
   rollup: {
     esbuild: {
       minify: true,
     },
   },
   hooks: {
+    // minify entries in dist/runtime folder
     'mkdist:entry:options': (_ctx, _entry, options) => {
       options.esbuild = {
         ...options.esbuild,
         minify: true,
       }
+    },
+    // tsconfig.json is not relevant to be shipped
+    'build:done': async () => {
+      await rm(resolve('dist/runtime/server/tsconfig.json'), { force: true })
     },
   },
 })
