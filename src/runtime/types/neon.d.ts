db70f713d153b97ec40a550ac833dfbad465838a
Alois Sečkár <ellrohir@seznam.cz>
2026-01-02 10:02:04 +0100

    feat: improved typing for module runtime options

--- src/runtime/types/neon.d.ts
+++ src/runtime/types/neon.d.ts
@@ -1,1 +1,25 @@
+import type { NeonSSLModeOption } from '../shared/types/neon'
+
 export * from '../shared/types/neon'
+
+declare module '@nuxt/schema' {
+  interface RuntimeConfig {
+    neonHost: string
+    neonUser: string
+    neonPass: string
+    neonDB: string
+    neonAllowedTables: string
+    neonAllowedQueries?: string
+  }
+
+  interface PublicRuntimeConfig {
+    neonDB: string
+    neonSSLMode: NeonSSLModeOption
+    neonDebugSQL: boolean
+    neonDebugRuntime: boolean
+    neonExposeEndpoints: boolean
+    neonExposeRawEndpoint: boolean
+  }
+}
+
+export {}
