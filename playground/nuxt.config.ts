a64ad7b9434a691a2e2ec9b19f3ab4d2303c270f
AloisSeckar <alois.seckar@gmail.com>
2026-07-29 15:20:32 +0200

    build: bump nuxt to v4.5.1 + update other deps

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,5 +2,5 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   modules: ['../src/module'],
-  compatibilityDate: '2026-03-21',
+  compatibilityDate: '2026-08-01',
 })
